d451e4976954c5d4eee2475d947e98b556dc25a4
Blood Marillion <bmarilli@ev-e6.kzn.21-school.ru>
2021-05-04 02:20:38 +0300

    add nginx

---
+++ services/setup.sh
@@ -0,0 +1,13 @@
+minikube stop
+minikube delete
+minikube start --driver=virtualbox --cpus=4 --memory=4G
+minikube addons enable dashboard
+minikube addons enable metallb
+# docker pull metallb/speaker:v0.8.2
+# docker pull metallb/controller:v0.8.2
+eval $(minikube -p minikube docker-env)
+kubectl apply -f srcs/configmap.yaml
+
+docker build -t nginx_image ./srcs/nginx/
+kubectl apply -f srcs/nginx/nginx.yaml
+minikube dashboard &
--- services/srcs/nginx/srcs/start.sh
+++ services/srcs/nginx/srcs/start.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-#nginx start
-rc-update add nginx default
-rc-service nginx start
-#service mysql start
-#service php7.3-fpm start
-sh
+#!/bin/sh
+
+nginx -g 'daemon off;'
