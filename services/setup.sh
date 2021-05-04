dc657a9dd2ea587a0b044535813790d585ecdfc9
Blood Marillion <bmarilli@et-a2.kzn.21-school.ru>
2021-05-05 02:32:28 +0300

    adds change

--- services/setup.sh
+++ services/setup.sh
@@ -7,8 +7,11 @@ minikube addons enable metallb
 # docker pull metallb/controller:v0.8.2
 eval $(minikube -p minikube docker-env)
 kubectl apply -f srcs/configmap.yaml
+
+
 docker build -t nginx_image ./srcs/nginx/
 kubectl apply -f srcs/nginx/nginx.yaml
+
 echo "\033[32m"
 echo "
 █──█──████──███──█──█──██─██
@@ -33,5 +36,34 @@ echo "
 ──────────────────────────█
 "
 echo "\033[37m"
+
+# docker build -t mysql_image ./srcs/mysql/
+# kubectl apply -f srcs/mysql/srcs/mysql.yaml
+
+echo "\033[32m"
+echo "
+█───█─██─██─███─████──█
+██─██──███──█───█──█──█
+█─█─█───█───███─█─██──█
+█───█───█─────█─█──█──█
+█───█───█───███─█████─███
+────────█───────────█
+
+\033[37m
+███─███
+─█──█
+─█──███
+─█────█
+███─███
+
+\\033[31m
+████─███─████─████──██─██
+█──█─█───█──█─█──██──███
+████─███─████─█──██───█
+█─█──█───█──█─█──██───█
+█─█──███─█──█─████────█
+──────────────────────█"
+echo "\033[37m"
+
 
 minikube dashboard &
