a7aa218cfad01a443b5c765a91bcf8973459bcec
ifanzilka <nabiyarov.fanzil@gmail.com>
2021-05-08 07:13:23 +0300

    final ftps

--- services/srcs/ftps/srcs/start.sh
+++ services/srcs/ftps/srcs/start.sh
@@ -1,7 +1,9 @@
 # for my usr
-# mkdir -p home/ftps/admin
-# chmod 777 home/ftps/admin
-# adduser --home "/home/ftps/admin" -D admin && echo "admin:admin" | chpasswd
-# adduser  -D admin1 && echo "admin1:admin1" | chpasswd
+# adds all users with path
+mkdir -p home/ftps/admin
+chmod 777 home/ftps/admin
+adduser --home "/home/ftps/admin" -D admin && echo "admin:admin" | chpasswd
+#adduser  -D admin1 && echo "admin1:admin1" | chpasswd
+
 rc-status && rc-service vsftpd start
 vsftpd /etc/vsftpd/vsftpd.conf
