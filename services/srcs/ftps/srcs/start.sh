557e8e00d6315db2227f99be5ce34d7b716440dd
Blood Marillion <bmarilli@et-d2.kzn.21-school.ru>
2021-05-08 06:51:42 +0300

    add usr ftps

--- services/srcs/ftps/srcs/start.sh
+++ services/srcs/ftps/srcs/start.sh
@@ -1,2 +1,7 @@
+# for my usr
+# mkdir -p home/ftps/admin
+# chmod 777 home/ftps/admin
+# adduser --home "/home/ftps/admin" -D admin && echo "admin:admin" | chpasswd
+# adduser  -D admin1 && echo "admin1:admin1" | chpasswd
 rc-status && rc-service vsftpd start
 vsftpd /etc/vsftpd/vsftpd.conf
